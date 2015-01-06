a703e3da16f7ddad33837991de2b2b274b53c22f
Erwin De Laat ( redN00ws ) <erwin.de.laat@cleveridge.org>
2015-01-06 20:12:54 +0100

    Create cl_menu.py

---
+++ lib/cl_menu.py
@@ -0,0 +1,34 @@
+#!/usr/lib/env python
+
+# IMPORTANT
+# ---------
+# This tool is for ethical testing purposes only.
+#
+# Cleveridge and its developers can't be held responsible 
+# for any misuse by users. 
+# Users have to act as permitted by local law rules.
+
+
+
+class Cl_menu:
+	
+	def __init__(self):
+		print "Menu Initiated"
+		
+			
+	
+	def donothing(self):
+		print "Menu : Do Nothing"
+	
+	def project_new(self):
+	    print "Menu : Project > New"
+	
+	def project_viewlogs(self):
+	    print "Menu : Project > View Logs"
+
+	def edit_configuration(self):
+		print "Menu : Edit > Configuration"
+	
+	def help_about(self):
+		print "Menu : Help > About"
+	
